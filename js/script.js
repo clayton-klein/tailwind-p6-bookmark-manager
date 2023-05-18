1eeb0562b63e9078785c81af37e04ac137908b7c
clayton-klein <csklein87@hotmail.com>
2023-05-18 17:56:53 -0300

    feat: add click handler to the hamburger btn/menu

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 const tabs = document.querySelectorAll(".tab");
 const panels = document.querySelectorAll(".panel");
+const logo = document.getElementById("logo");
+const hamburgerBtn = document.getElementById("menu-btn");
+const mobileMenu = document.getElementById("menu");
 
 tabs.forEach((tab) => tab.addEventListener("click", onTabClick));
+hamburgerBtn.addEventListener("click", navToggle);
 
 function onTabClick(event) {
   // "Deactivate" all tabs
@@ -22,7 +26,7 @@ function onTabClick(event) {
   // Getting the tab's custom attribute value that will match the class of its respective panel
   const matchedAttributeClass = event.target.getAttribute("data-target");
 
-  // Show target panel
+  // Show target panel (remove class "hidden" & add class "flex")
   document
     // div with the panels
     .getElementById("panels")
@@ -30,4 +34,24 @@ function onTabClick(event) {
     // "data-target" of their respective tabs.
     .getElementsByClassName(matchedAttributeClass)[0]
     .classList.remove("hidden");
+
+  document
+    .getElementById("panels")
+    .getElementsByClassName(matchedAttributeClass)[0]
+    .classList.add("flex");
+}
+
+// ============== Toggle mobile menu =====================
+function navToggle() {
+  hamburgerBtn.classList.toggle("open");
+  mobileMenu.classList.toggle("flex");
+  mobileMenu.classList.toggle("hidden");
+
+  if (mobileMenu.classList.contains("flex")) {
+    // white logo
+    logo.setAttribute("src", "./images/logo-bookmark-footer.svg");
+  } else {
+    // blackish logo
+    logo.setAttribute("src", "./images/logo-bookmark.svg");
+  }
 }
